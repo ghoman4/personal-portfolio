51072d652f0b75e2ef2767c970e85f08782d6ce3
ghoman4 <griffhoho@gmail.com>
2024-12-12 16:53:47 -0500

    Nvm, now considered done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "../styles/components/NavBar.scss";
 
 export enum CurrentPage {
     HOME = 'HOME',
