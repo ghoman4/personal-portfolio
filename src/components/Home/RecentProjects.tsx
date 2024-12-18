543b1c722d65761219c82b67c8e161aa762905e1
ghoman4 <griffhoho@gmail.com>
2024-12-18 15:08:30 -0500

    Finished projects

--- src/components/Home/RecentProjects.tsx
+++ src/components/Home/RecentProjects.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "../../styles/pages/Home.scss";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
 import localifyPreview from "../../assets/localify-preview.png";
+import charityTreePreview from "../../assets/charity-tree-preview.png";
+import porchfestPreview from "../../assets/porchfest-preview.png";
 
 const RecentProjects: React.FC = () => {
     return (
         <section className="recent-projects">
-            {/* Project Card */}
             <div className="project">
                 <div className="project__image">
                     <img src={localifyPreview} alt="Localify Preview"/>
                 </div>
                 <div className="project__content">
-                    <h5 className="project__localify-title">Localify.org</h5>
-                    <h6 className="project__role">Frontend Developer</h6>
-                    <p className="project__duration">May 2022 - Present</p>
+                    <h4 className="project__title localify-title">Localify.org</h4>
+                    <h5 className="project__role">Frontend Developer</h5>
+                    <h5 className="project__duration">May 2022 - Present</h5>
                     <p className="project__description">
                         A platform for discovering local music, offering personalized artist recommendations and
                         promoting community engagement.
@@ -23,7 +26,58 @@ const RecentProjects: React.FC = () => {
                         <span className="tag">Typescript</span>
                         <span className="tag">Web App</span>
                     </div>
-                    <button className="project__button">View More</button>
+                    <span className="project__button localify-button">
+                        <h6>View More</h6>
+                        <ChevronRightIcon/>
+                    </span>
+                </div>
+            </div>
+            <div className="project">
+                <div className="project__image">
+                    <img src={charityTreePreview} alt="Charity Tree Preview"/>
+                </div>
+                <div className="project__content">
+                    <h4 className="project__title charity-tree-title">Charity Tree</h4>
+                    <h5 className="project__role">Full-Stack Developer</h5>
+                    <h5 className="project__duration">February 2024 - Present</h5>
+                    <p className="project__description">
+                        An application designed to connect local charities with individuals and
+                        organizations looking to contribute, streamlining the process of discovering
+                        and supporting charitable causes within the community.
+                    </p>
+                    <div className="project__tags">
+                        <span className="tag">React JS</span>
+                        <span className="tag">Springboot</span>
+                        <span className="tag">PostgreSQL</span>
+                    </div>
+                    <span className="project__button charity-tree-button">
+                        <h6>View More</h6>
+                        <ChevronRightIcon/>
+                    </span>
+                </div>
+            </div>
+            <div className="project">
+                <div className="project__image">
+                    <img src={porchfestPreview} alt="Porch Fest Preview"/>
+                </div>
+                <div className="project__content">
+                    <h4 className="project__title porchfest-title">Porchfest</h4>
+                    <h5 className="project__role">Frontend Developer</h5>
+                    <h5 className="project__duration">August 2024 - Present</h5>
+                    <p className="project__description">
+                        An app developed to enhance the PorchFest experience, allowing users to explore live music
+                        events with a location-based map that tracks nearby performances without requiring user
+                        interaction.
+                    </p>
+                    <div className="project__tags">
+                        <span className="tag">Swift UI</span>
+                        <span className="tag">Mobile App</span>
+                        <span className="tag">MapKit</span>
+                    </div>
+                    <span className="project__button porchfest-button">
+                        <h6>View More</h6>
+                        <ChevronRightIcon/>
+                    </span>
                 </div>
             </div>
         </section>
