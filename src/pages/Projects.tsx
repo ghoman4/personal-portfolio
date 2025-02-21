a049ffbee672f295b7a480b0f20aeb615af02537
ghoman4 <griffhoho@gmail.com>
2025-02-21 15:57:36 -0500

    Projects basic setup complete

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import "../styles/pages/Projects.scss";
 
 const Projects: React.FC = () => {
-
     return (
-        <div>
+        <div className="projects">
+            <div className="container">
+                <h1 className="title">Projects</h1>
+                <div className="card-container">
+                    {[1, 2, 3, 4, 5, 6].map(num => (
+                        <div className="card" key={num}>
+                            <h6>{num}</h6>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Projects;
