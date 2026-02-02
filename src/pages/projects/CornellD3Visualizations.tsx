a7e76aa3b75a3e7b56d6f46adbd9220ddf36aafd
ghoman4 <griffhoho@gmail.com>
2026-02-02 16:15:32 -0500

    Force GitHub Pages rebuild

---
+++ src/pages/projects/CornellD3Visualizations.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles/pages/ProjectPage.scss";
+
+const CornellProjects: React.FC = () => {
+
+    return (
+        <div>
+            Meow meow
+        </div>
+    )
+}
+
+export default CornellProjects;
